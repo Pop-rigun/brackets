a1e083a207a267abf8be11cd95aa5175f4017c80
Pop-rigun <72011576+Pop-rigun@users.noreply.github.com>
2021-03-15 21:27:54 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.lenght & 1) return false;
+  let position, length;
+  do{
+    length = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      while (~(position = str.indexOf(bracketsConfig[i].join('')))) {
+          str = str.substr(0, position) + str.substr(position + 2, str.length);
+        }
+    }
+  }while(str.length < length && str.length != 0);
+  return !str.length;
+
 }
